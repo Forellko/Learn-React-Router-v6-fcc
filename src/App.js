64e8469eba2fbe76b1ba558902f6f5ea93c28a47
forellko <dragonforce97q@gmail.com>
2024-04-05 14:08:33 +0300

    Create number component with useParams

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useParams } from 'react-router-dom';
 import Navbar from './components/Navbar';
 
 const Home = () => {
   return <h1>Home</h1>;
+};
+
+const Number = () => {
+  const number = useParams();
+  return <h1>{number.num}</h1>;
 };
 
 const About = () => {
@@ -19,6 +24,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navbar />}>
           <Route path="/" element={<Home />} />
+          <Route path="/number/:num" element={<Number />} />
           <Route path="/about" element={<About />} />
           <Route path="*" element={<NotFound />} />
         </Route>
