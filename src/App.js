b99f4e6a94157c843182c33442ae784d25b33b66
forellko <dragonforce97q@gmail.com>
2024-04-05 14:35:09 +0300

    Create GitHubUser with search users

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes, useParams } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import { useEffect, useState } from 'react';
 
 const Home = () => {
   return <h1>Home</h1>;
@@ -8,6 +9,29 @@ const Home = () => {
 const Number = () => {
   const number = useParams();
   return <h1>{number.num}</h1>;
+};
+
+const fetchGitHubUser = async ({ username, setUser }) => {
+  const response = await fetch(`https://api.github.com/users/${username}`);
+  const user = await response.json();
+  console.log(user);
+  setUser(user);
+};
+
+const GitHubUser = () => {
+  const [user, setUser] = useState();
+  const paramUser = useParams();
+
+  useEffect(() => {
+    fetchGitHubUser({ username: paramUser.username, setUser });
+  }, []);
+
+  return (
+    <>
+      <img src={user?.avatar_url} alt="avatar" />
+      <h2>{user?.login}</h2>
+    </>
+  );
 };
 
 const About = () => {
@@ -25,6 +49,7 @@ function App() {
         <Route path="/" element={<Navbar />}>
           <Route path="/" element={<Home />} />
           <Route path="/number/:num" element={<Number />} />
+          <Route path="/user/:username" element={<GitHubUser />} />
           <Route path="/about" element={<About />} />
           <Route path="*" element={<NotFound />} />
         </Route>
