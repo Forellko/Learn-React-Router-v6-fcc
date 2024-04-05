771a639d6ca567d47a85a0ca4c96f1d19271acbf
forellko <dragonforce97q@gmail.com>
2024-04-05 13:09:19 +0300

    Create Routes and Route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
+const Home = () => {
+  return <h1>Home</h1>;
+};
+
+const About = () => {
+  return <h1>About</h1>;
+};
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
